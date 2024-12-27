3c4dbc1100ea1981a4fdcabf4066ac22b4bc812d
Andres Maneiro <amaneiro7@gmail.com>
2024-12-27 01:00:43 -0400

    Cambio en el nombre de la ruta employee

--- src/Contexts/Device/Device/application/DeviceFinderAll.ts
+++ src/Contexts/Device/Device/application/DeviceFinderAll.ts
@@ -2,9 +2,9 @@ import { EmployeePrimitives } from '../../../employee/Employee/domain/Employee'
 import { type Repository } from '../../../Shared/domain/Repository'
 
 export class SearchAllEmployees {
-  constructor (private readonly repository: Repository) {}
+  constructor(private readonly repository: Repository) { }
 
-  async search (): Promise<EmployeePrimitives[]> {
+  async search(): Promise<EmployeePrimitives[]> {
     return await this.repository.employee.searchAll()
   }
 }
