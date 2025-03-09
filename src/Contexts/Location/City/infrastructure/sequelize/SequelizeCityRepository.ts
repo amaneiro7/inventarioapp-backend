9f0c9a0354f64c6d5faf8e07393aac559169aab3
Andres Maneiro <amaneiro7@gmail.com>
2025-03-09 15:42:27 -0400

    modificacion de los joins en city

--- src/Contexts/Location/City/infrastructure/sequelize/SequelizeCityRepository.ts
+++ src/Contexts/Location/City/infrastructure/sequelize/SequelizeCityRepository.ts
@@ -5,11 +5,11 @@ import { type Primitives } from '../../../../Shared/domain/value-object/Primitiv
 import { type CityPrimitives, type CityDto } from '../../domain/City.dto'
 import { type CityId } from '../../domain/CityId'
 import { type CityRepository } from '../../domain/CityRepository'
+import { type CityName } from '../../domain/CityName'
 import { TimeTolive } from '../../../../Shared/domain/CacheRepository'
 import { CriteriaToSequelizeConverter } from '../../../../Shared/infrastructure/criteria/CriteriaToSequelizeConverter'
 import { CityModel } from './CitySchema'
 import { CityAssociation } from './CityAssociation'
-import { CityName } from '../../domain/CityName'
 
 export class SequelizeCityRepository extends CriteriaToSequelizeConverter implements CityRepository {
 	private readonly cacheKey: string = 'cities'
@@ -18,7 +18,6 @@ export class SequelizeCityRepository extends CriteriaToSequelizeConverter implem
 	}
 	async searchAll(criteria: Criteria): Promise<ResponseDB<CityDto>> {
 		const options = CityAssociation.converFilter(criteria, this.convert(criteria))
-		options.include = ['state']
 		return await this.cache.getCachedData({
 			cacheKey: this.cacheKey,
 			criteria,
