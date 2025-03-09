e901ee3d8a95cec242d49a8bd6b4c4b4bac66eaf
Andres Maneiro <amaneiro7@gmail.com>
2025-03-09 15:41:59 -0400

    joins en site

--- src/Contexts/Location/Site/infrastructure/sequelize/SequelizeSiteRepository.ts
+++ src/Contexts/Location/Site/infrastructure/sequelize/SequelizeSiteRepository.ts
@@ -1,12 +1,12 @@
-import { TimeTolive } from '../../../../Shared/domain/CacheRepository'
 import { type CacheService } from '../../../../Shared/domain/CacheService'
 import { type Criteria } from '../../../../Shared/domain/criteria/Criteria'
 import { type ResponseDB } from '../../../../Shared/domain/ResponseType'
 import { type Primitives } from '../../../../Shared/domain/value-object/Primitives'
-import { CriteriaToSequelizeConverter } from '../../../../Shared/infrastructure/criteria/CriteriaToSequelizeConverter'
 import { type SitePrimitives, type SiteDto } from '../../domain/Site.dto'
 import { type SiteId } from '../../domain/SiteId'
 import { type SiteRepository } from '../../domain/SiteRepository'
+import { TimeTolive } from '../../../../Shared/domain/CacheRepository'
+import { CriteriaToSequelizeConverter } from '../../../../Shared/infrastructure/criteria/CriteriaToSequelizeConverter'
 import { SiteModels } from './SiteSchema'
 
 export class SequelizeSiteRepository extends CriteriaToSequelizeConverter implements SiteRepository {
@@ -37,7 +37,21 @@ export class SequelizeSiteRepository extends CriteriaToSequelizeConverter implem
 	}
 
 	async searchById(id: Primitives<SiteId>): Promise<SiteDto | null> {
-		return (await SiteModels.findByPk(id)) ?? null
+		return (
+			(await SiteModels.findByPk(id, {
+				include: [
+					{
+						association: 'city',
+						include: [
+							{
+								association: 'state',
+								include: ['region']
+							}
+						]
+					}
+				]
+			})) ?? null
+		)
 	}
 
 	async save(payload: SitePrimitives): Promise<void> {
