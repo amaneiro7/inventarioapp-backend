d1174225ab6f6428e1ec8abd381a99e58cddcdb9
Andres Maneiro <amaneiro7@gmail.com>
2025-05-02 06:40:10 -0400

    phone numbers seed

---
+++ seeders/20250501051351-add-agency-phone.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { employeeAgencyPhone } = require('./employee/employeeAgencyPhone');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    for (const employee of employeeAgencyPhone) {
+      const existingEmployee = await queryInterface.rawSelect(
+        'employees',
+        {
+          where: {
+            user_name: {
+              [Sequelize.Op.iLike]: `${employee.userName.toLowerCase()}`
+            }
+          }
+        },
+        ['id']
+      )
+      if (existingEmployee) {
+        await queryInterface.bulkUpdate(
+          'employees',
+          {
+            phone: employee.extension,
+          },
+          {
+            id: existingEmployee
+          }
+        )
+      }
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
