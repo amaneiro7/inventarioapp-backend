6743cca5d061687ee32cfacd0623505c6a103ead
Andres Maneiro <amaneiro7@gmail.com>
2025-06-04 01:17:22 -0400

    seed migration

--- src/Contexts/Device/Device/application/PingService.ts
+++ src/Contexts/Device/Device/application/PingService.ts
@@ -14,30 +14,6 @@ interface PingResult {
 }
 export class PingService {
 	constructor(private readonly logger: Logger) {}
-
-	/**
-	 * Pings a given IP address and returns a structured result.
-	 * Throws an error if the host is unreachable or if the ping command fails.
-	 */
-	// async pingIp({ ipAddress }: { ipAddress: string }): Promise<string> {
-	// 	return new Promise((resolve, reject) => {
-	// 		let command = ''
-	// 		const osPlatform = platform()
-	// 		if (osPlatform.startsWith('win')) {
-	// 			command = `ping -n 1 -w 1000 ${ipAddress}` // Windows: 1 ping, 1000ms timeout
-	// 		} else {
-	// 			command = `ping -c 1 -W 1 ${ipAddress}` // Linux/macOS: 1 ping, 1 second timeout
-	// 		}
-
-	// 		exec(command, (error, stdout, stderr) => {
-	// 			if (error || stderr) {
-	// 				reject(error)
-	// 				return
-	// 			}
-	// 			return resolve(stdout)
-	// 		})
-	// 	})
-	// }
 	async pingIp({ ipAddress, enableLogging = false }: { ipAddress: string; enableLogging?: boolean }): Promise<void> {
 		let command = ''
 		let pingArgs: string[] = []
