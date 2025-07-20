9544f80dc01d89fc31f77e0a720ab2bd683bea5b
Andres Maneiro <amaneiro7@gmail.com>
2025-07-20 16:32:00 -0400

    configuracion del linter actualizado

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,17 +5,10 @@ import json from "@eslint/json";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 
-export default defineConfig(
-  [
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
-    tseslint.configs.recommended,
-    { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-    { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  ],
-  [globalIgnores([
-    "./src/Contexts/Shared/infrastructure/persistance/Sequelize/seeders/*",
-    "./src/Contexts/Shared/infrastructure/persistance/Sequelize/migrations/*",
-    "./src/Contexts/Shared/infrastructure/persistance/Sequelize/config.js"
-  ])]
-);
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+], [globalIgnores(['node_modules/*', 'dist', 'config.js', '**/migrations/*', '**/seeders/*'])]);
